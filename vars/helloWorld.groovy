d0572e6b9c1d2b90ad17915cebeea4c615259b4e
Rohit-Nakhate <130633843+Rohit-Nakhate@users.noreply.github.com>
2023-06-01 12:31:18 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,9 @@
 def call (){
 	echo "< WELCOME TO A-DEVOPS > !!!"
-	bat "ping a-devops.com"
+	  stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                       url: "https://github.com/Rohit-Nakhate/shared-LIB.git"
+               }
+           }
 }
