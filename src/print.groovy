267a9c46208dec926fcba9204e2c0fe7877a0f54
Rohit-Nakhate <130633843+Rohit-Nakhate@users.noreply.github.com>
2023-06-01 11:42:44 +0530

    Update and rename helloWorld.groovy to print.groovy

---
+++ src/print.groovy
@@ -0,0 +1,7 @@
+def call (){
+	echo "< WELCOME TO A-DEVOPS > !!!"
+	echo "< AUTOMATION  > !!!"
+	echo "< JENKINS CODE AS CONFIGURATION > !!!"
+	echo "< SHARED LIBRARY > !!!"
+	bat"PING a-devops.com "
+}
